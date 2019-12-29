dd55f0fdf4a35511c03b0e5bf9a1d61994eea488
djangovikash <vicckyvikash341@gmail.com>
2019-12-29 14:12:43 +0530

    ListView class is added inside views.py   *Model Item is added inside model.py

--- CyberBoxer/testapp/admin.py
+++ CyberBoxer/testapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from testapp.models import Item
 # Register your models here.
+class ItemAdmin(admin.ModelAdmin):
+    list_display=['name','quantity','imag']
+admin.site.register(Item,ItemAdmin)
+ 
--- CyberBoxer/testapp/views.py
+++ CyberBoxer/testapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from testapp.models import Item
 # Create your views here.
+class List_view(ListView):
+    model = Item
+    template_name="testapp/list.html"
+    context_object_name= "Item_list"
